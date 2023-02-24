2d5eb21f91b1515ced840a3c8e491255d36e3ae3
andremuriae@yahoo.com.br <andrerorizfernandes>
2023-02-23 21:48:04 -0300

    Add project files.

---
+++ PrimeiroProjeto/MyCustomClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    public class MyCustomClass
+    {
+        //1. atributo privado com getter e setter
+        private int age;
+        public void SetAge(int pAge)
+        {
+            age = pAge;
+        }
+
+        public int GetAge()
+        {
+            return age;
+        }
+
+        //2. propertie
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        //3. auto-propertie
+        public int Number { get; set; }
+
+        public int Multiply(int pNumber)
+        {
+            return (Number * pNumber);
+        }
+
+        public MyCustomClass()
+        {
+            Number = 0;
+        }
+    }
+}
---
+++ PrimeiroProjeto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeiroProjeto
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCustomClass myClass = new MyCustomClass();
+    
+            myClass.Number = 27;
+            int result = myClass.Multiply(4);
+            myClass.SetAge(50);
+            myClass.Name = "Fulano de tal";
+
+            Console.WriteLine("O resultado da multiplicação é: " + result);
+            Console.WriteLine("A idade é: " + myClass.GetAge());
+            Console.WriteLine("O nome é: " + myClass.Name);
+            Console.ReadKey();
+        }
+    }
+}
